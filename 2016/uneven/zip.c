5545e9cbe39645908cf77d7276239ed1d26eb01a
samclarke2012 <clarke.sam1@gmail.com>
2016-06-03 08:27:14 +1000

    comments

--- 2016/uneven/zip.c
+++ 2016/uneven/zip.c
@@ -22,23 +22,35 @@
 void zip (list listA, list listB, list zipped) {
     link p1;
     link p2;
+    // Go ahead and point to the head of A
     zipped->head = listA->head;
+    // Keep a point to it
     link p3 = zipped->head;
+    // If there's work to be done
     if(listA->head != NULL){
+        // Setup pointers
         p1 = listA->head->next;
         p2 = listB->head;
+        // While there's stuff in either
         while(p1 != NULL || p2 != NULL){
+            // We've already primed zipped
+            // with listA, so start with listB
             if(p2 != NULL){
+                // Append the node
                 p3->next = p2;
+                // Increment the pointers
                 p2 = p2->next;
                 p3 = p3->next;
             }
+            // Then, same goes for listA
             if(p1 != NULL){
                 p3->next = p1;
                 p1 = p1->next;
                 p3 = p3->next;
             }
         }
+        // Finish off the new list 
+        // with a NULL pointer.
         p3->next = NULL;
     }
 
